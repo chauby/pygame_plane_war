c5cad590be85e2379d4e7b28bd110d860524fd86
chauby <chaubyZou@163.com>
2017-05-01 11:38:14 +0800

    飞机大战测试，具有两种类型的敌机，一种类型的奖励，暂时不能发射子弹，只能躲避

--- playGame.py
+++ playGame.py
@@ -8,47 +8,86 @@ import random
 # import pygame.locals for easier access to key coordinates
 from pygame.locals import *
 
+#定义游戏的屏幕大小
+width = 1200
+hight = 800
+
 #定义Player类
 class Player(pygame.sprite.Sprite):
 	def __init__(self):
 		super(Player, self).__init__()
-		self.image = pygame.image.load('jet.png').convert()
+		image = pygame.image.load('GodPlane2.png').convert()
+		self.image = pygame.transform.scale(image, (64, 64))
 		self.image.set_colorkey((255, 255, 255), RLEACCEL)
-		self.rect = self.image.get_rect()
+		self.rect = self.image.get_rect(center = (width/2, 600))#set the plaer's init position
 
 	def update(self, pressed_keys):
 		if pressed_keys[K_UP]:
-			self.rect.move_ip(0, -5)
+			self.rect.move_ip(0, -2)
 		if pressed_keys[K_DOWN]:
-			self.rect.move_ip(0, 5)
+			self.rect.move_ip(0, 2)
 		if pressed_keys[K_LEFT]:
-			self.rect.move_ip(-5, 0)
+			self.rect.move_ip(-2, 0)
 		if pressed_keys[K_RIGHT]:
-			self.rect.move_ip(5, 0)
+			self.rect.move_ip(2, 0)
 
 		#限制plaer的移动范围，不能超出屏幕
 		if self.rect.left < 0:
 			self.rect.left = 0
-		elif self.rect.right > 800:
-			self.rect.right = 800
+		elif self.rect.right > width:
+			self.rect.right = width
 		if self.rect.top < 0:
 			self.rect.top = 0
-		elif self.rect.bottom > 600:
-			self.rect.bottom = 600
+		elif self.rect.bottom > hight:
+			self.rect.bottom = hight 
 
+#定义血点奖励类
+class RewardBlood(pygame.sprite.Sprite):
+	def __init__(self):
+		super(RewardBlood, self).__init__()
+		image = pygame.image.load('sphere2.png').convert()
+		self.image = pygame.transform.scale(image, (32, 32))
+		self.image.set_colorkey((255, 255, 255), RLEACCEL)
+		self.rect = self.image.get_rect(center = (random.randint(0, width), 0))
+		self.speed = random.randint(1, 2)
+
+	def update(self):
+		self.rect.move_ip(0, self.speed)
+		if self.rect.bottom > hight:
+			self.kill()
+
+#定义敌人类
 class Enemy(pygame.sprite.Sprite):
 	def __init__(self):
 		super(Enemy, self).__init__()
-		self.image = pygame.image.load('missile.png').convert()
-		self.init_position = [10, 100, 200, 300, 400, 500, 580]
+		if random.randint(1,2) == 1:
+			image = pygame.image.load('JpPlane2.png').convert()
+		else:
+			image = pygame.image.load('GreenPlane2.png').convert()
+		self.image = pygame.transform.scale(image, (32, 32))
+		self.init_position = [10, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1190]
 		self.image.set_colorkey((255, 255, 255), RLEACCEL)
-		self.rect = self.image.get_rect(center = (820, self.init_position[random.randint(0, 6)]))
-		# self.speed = random.randint(1, 2)
-		self.speed = 1
+		self.rect = self.image.get_rect(center = (self.init_position[random.randint(0, 12)], 0))
+		self.speed = random.randint(1, 2)
 
 	def update(self):
-		self.rect.move_ip(-self.speed, 0)
-		if self.rect.right < 0:
+		self.rect.move_ip(0, self.speed)
+		if self.rect.bottom > hight:
+			self.kill()
+
+#定义云朵类
+class Cloud(pygame.sprite.Sprite):
+	"""docstring for Cloud"""
+	def __init__(self):
+		super(Cloud, self).__init__()
+		self.image = pygame.image.load('cloud.png').convert()
+		self.image.set_colorkey((0, 0, 0), RLEACCEL)
+		self.rect = self.image.get_rect(center = (random.randint(0, width), 0))
+		self.speed = random.randint(1,3)
+
+	def update(self):
+		self.rect.move_ip(0, self.speed)
+		if self.rect.bottom > hight:
 			self.kill()
 
 # initialize pygame
@@ -56,7 +95,7 @@ pygame.init()
 
 # create the screen object
 # here we pass it a size of 800x600
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((width, hight))
 
 #创建游戏背景
 background = pygame.Surface(screen.get_size())
@@ -67,12 +106,20 @@ ADDENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDENEMY, 500)
 ADDCLOUD = pygame.USEREVENT + 2
 pygame.time.set_timer(ADDCLOUD, 1000)
+ADDREWARD = pygame.USEREVENT + 3
+pygame.time.set_timer(ADDREWARD, 1000)
 
 #创建plaer
 player = Player()
 
 #创建敌人
 enemies = pygame.sprite.Group()
+
+#创建云彩
+clouds = pygame.sprite.Group()
+
+#创建奖励
+rewardBloods = pygame.sprite.Group()
 
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
@@ -90,19 +137,29 @@ while running:
 			new_enemy = Enemy()
 			enemies.add(new_enemy)
 			all_sprites.add(new_enemy)
+		elif event.type == ADDCLOUD:
+			new_cloud = Cloud()
+			clouds.add(new_cloud)
+			all_sprites.add(new_cloud)
+		elif event.type == ADDREWARD:
+			new_reward = RewardBlood()
+			rewardBloods.add(new_reward)
+			all_sprites.add(new_reward)
 
 	screen.blit(background, (0, 0))
+	clouds.update()
+	enemies.update()
+	rewardBloods.update()
 	pressed_keys = pygame.key.get_pressed()
 	player.update(pressed_keys)
-	enemies.update()
 
 	for entity in all_sprites:
 		screen.blit(entity.image, entity.rect)
 
 	if pygame.sprite.spritecollideany(player, enemies):
-		print "Game over"
 		player.kill()
 		running = False
+		print "Game over"
 
 	#将两次flip之间的修改更新到整个屏幕
 	pygame.display.flip()
